31aeab4ae59de9e74c32575f7847f370779e899d
Scott Young <scott.young@getbridge.com>
2022-09-21 17:26:21 -0600

    mobile styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Header from '../components/Header'
 import CountdownTimer from '../components/CountdownTimer'
 import RsvpForm from '../components/RsvpForm';
+import QuoteContainer from '../components/QuoteContainer';
 
 
 const Home = () => {
   const EVENT_DATE = new Date("October 13, 2023 15:00:00:000")
+  const QUOTE =
+  "Did my heart love till now? Forswear it, sight, For I ne'er saw true beauty till this night.";
 
   return(
     <div id="home">
       <Header />
       <CountdownTimer targetDate={EVENT_DATE} title="The big day"/>
+      <QuoteContainer quote={QUOTE} author="William Shakespeare"/>
       <RsvpForm />
     </div>
   )
