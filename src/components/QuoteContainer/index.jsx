9bee96d25046bce9d39421ad58a2adaff9b1b4cf
Scott Young <scott.young@getbridge.com>
2022-09-21 17:25:55 -0600

    add quote

---
+++ src/components/QuoteContainer/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+
+const QuoteContainer = ({ quote, author }) => {
+  return (
+    <div className="quote-container"></div>
+  );
+};
+
+export default QuoteContainer;
