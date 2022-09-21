0e2ec6dedc7578fcc832e36944fb1e669d6a23b6
Scott Young <scott.young@getbridge.com>
2022-09-21 15:23:14 -0600

    adjust link functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Header from '../components/Header'
 import CountdownTimer from '../components/CountdownTimer'
+import RsvpForm from '../components/RsvpForm';
 
 
 const Home = () => {
   const EVENT_DATE = new Date("October 13, 2023 15:00:00:000")
 
   return(
-    <div>
+    <div id="home">
       <Header />
       <CountdownTimer targetDate={EVENT_DATE} title="The big day"/>
+      <RsvpForm />
     </div>
   )
 }
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import { Link, Outlet } from 'react-router-dom'
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Root = () => {
-  return(
+  return (
     <div className="root">
       <div className="nav">
-        <Link to="/" className="nav-item">Home</Link>
-        <Link to="/gallery" className="nav-item">Gallery</Link>
+        <Link
+          to="/"
+          className="nav-item"
+          onClick={() => window.location.replace("/#home")}
+        >
+          Home
+        </Link>
+        <Link to="/gallery" className="nav-item">
+          Gallery
+        </Link>
       </div>
       <Outlet />
     </div>
-  )
-}
+  );
+};
 
-export default Root
+export default Root;
