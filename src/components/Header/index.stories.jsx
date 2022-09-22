d629e6b4f84b59073a66199e5bb0a4449e36b77b
Scott Young <scott.young@getbridge.com>
2022-09-22 13:25:59 -0600

    add storybook for header

---
+++ src/components/Header/index.stories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Header from "./index";
+
+storiesOf("Header", module).add("Modal", () => {
+  return <Header eventDate={new Date("October 13, 2023 15:00:00:000")} />;
+});
