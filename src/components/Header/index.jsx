8f2854c0cf32983f59b70019fb26e2a42747c620
Scott Young <scott.young@getbridge.com>
2022-09-22 14:39:08 -0600

    header update

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import './index.css'
+
 const Header = ({ eventDate }) => {
   return (
     <div className="header">
       <div className="center">
-        <h1 className="title">Save The Date!</h1>
+        <span>Come celebrate the union of</span>
+        <h1 className="title">Scott Young & Alisia Harry</h1>
         <h2 className="subtitle">Friday, October 13th 2023</h2>
         {!(new Date() === eventDate) && (
           <button
