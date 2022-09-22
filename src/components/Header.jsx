0bd4b94fc91ce18766de645d157c6452f26b0677
Scott Young <scott.young@getbridge.com>
2022-09-22 10:47:24 -0600

    fix button on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ eventDate }) => {
       <div className="center">
         <h1 className="title">Save The Date!</h1>
         <h2 className="subtitle">Friday, October 13th 2023</h2>
-        {!new Date() === eventDate && (
+        {!(new Date() === eventDate) && (
           <button
             className="rsvp-button"
             onClick={() => window.location.replace("/#rsvp")}
