6826c1efb8dac584d50ddca50e2c320c983f6877
Scott Young <scott.young@getbridge.com>
2022-09-23 12:13:56 -0600

    add new component script

---
+++ bin/scripts/new_component.sh
@@ -0,0 +1,14 @@
+mkdir src/components/$1
+touch src/components/$1/index.css
+touch src/components/$1/index.jsx
+echo "import React from 'react'
+
+const $1 = () => {
+  return(
+    <div></div>
+  )
+}
+
+export default $1" >> src/components/$1/index.jsx
+
+touch src/components/$1/index.stories.jsx
