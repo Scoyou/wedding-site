2b16399c71486c84dee3e9ebc09436d4368ac602
Scott Young <scott.young@getbridge.com>
2022-09-23 09:56:38 -0600

    base cloudinary setup w/ example image

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {AdvancedImage} from '@cloudinary/react';
+import {Cloudinary} from "@cloudinary/url-gen";
 
 const Gallery = () => {
+
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: 'dd73hevmu'
+    }
+  });
+
+  const myImage = cld.image('cld-sample'); 
+  
   return(
-    <div>Coming soon!</div>
+    <div id="gallery">
+       <AdvancedImage cldImg={myImage} />
+    </div>
   )
 }
 
