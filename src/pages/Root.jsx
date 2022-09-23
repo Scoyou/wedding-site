2cb1de66e5eeb5ec61c90efd9b613d53b37a6262
Scott Young <scott.young@getbridge.com>
2022-09-23 14:52:03 -0600

    npm package imported, not working

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,30 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import { Link, Outlet } from "react-router-dom";
+import {Navbar} from "responsive-nav-react";
 
 const Root = () => {
   return (
     <div className="root">
-      <Navbar />
+      <Navbar
+        navTitle="Scott & Alisia"
+        menuIcon={process.env.PUBLIC_URL + "/menu-icon.png"}
+        iconHeight="20"
+        iconWidth="20"
+      >
+        <>
+          <Link to="/gallery" className="nav-item">
+            Gallery
+          </Link>
+
+          <Link
+            to="/"
+            className="nav-item"
+            onClick={() => window.location.replace("/#home")}
+          >
+            Home
+          </Link>
+        </>
+      </Navbar>
       <Outlet />
     </div>
   );
