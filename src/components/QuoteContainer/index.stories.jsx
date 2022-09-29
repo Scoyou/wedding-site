6bc96d145ba9f34c69349447dabf622a7b7ce26e
Scott Young <scott.young@getbridge.com>
2022-09-29 10:22:23 -0600

    styling changes

--- src/components/QuoteContainer/index.stories.jsx
+++ src/components/QuoteContainer/index.stories.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import QuoteContainer from "./index";
 
-const QUOTE =
-  "Did my heart love till now? Forswear it, sight, For I ne'er saw true beauty till this night.";
-
 storiesOf("QuoteContainer", module).add("Quote Container", () => {
-  return <QuoteContainer quote={QUOTE} author="William Shakespeare" />;
+  return <QuoteContainer />;
 });
