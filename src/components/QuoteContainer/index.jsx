8822961503fec6fdd12d4a8b79d6063acca9eaa4
Scott Young <scott.young@getbridge.com>
2022-10-03 15:46:10 -0600

    quote container changes

--- src/components/QuoteContainer/index.jsx
+++ src/components/QuoteContainer/index.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 import './index.css'
 
 const QuoteContainer = () => {
-  return <div className="quote-container" data-testid="quote-container"></div>
+  return (
+    <div className="quote-container" data-testid="quote-container">
+      <img
+        className="quote-image"
+        src={process.env.PUBLIC_URL + '/quote-image.png'}
+        alt="Shakespeare quote"
+      />
+      <img
+        className="quote-image"
+        src={process.env.PUBLIC_URL + '/marry-me-sign.jpeg'}
+        alt="Marry me sign"
+      />
+    </div>
+  )
 }
 
 export default QuoteContainer
