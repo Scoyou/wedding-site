1470d5b0f175536b3177746596942a7e03406644
Scott Young <scott.young@getbridge.com>
2022-10-03 08:57:15 -0600

    fill stories info with bin script

--- bin/scripts/new_component.sh
+++ bin/scripts/new_component.sh
@@ -1,9 +1,11 @@
+# Create component files
 mkdir src/components/$1
 
 touch src/components/$1/index.css
 touch src/components/$1/index.jsx
 touch src/components/$1/index.stories.jsx
 
+# Fill index.jsx
 echo "import React from 'react'
 
 const $1 = () => {
@@ -13,3 +15,12 @@ const $1 = () => {
 }
 
 export default $1" >> src/components/$1/index.jsx
+
+# Fill index.stories.jsx
+echo "import React from 'react';
+import { storiesOf } from '@storybook/react';
+import $1 from './index';
+
+storiesOf('$1', module).add('$1', () => {
+  return <$1 />;
+});" >> src/components/$1/index.stories.jsx
