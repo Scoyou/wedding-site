e6152c24bd3ed76ec655e9128f56e4a75218914a
Scott Young <scott.young@getbridge.com>
2022-10-03 12:14:57 -0600

    fix indentation when creating new components

--- bin/scripts/new_component.sh
+++ bin/scripts/new_component.sh
@@ -9,39 +9,39 @@ if [ -z "$2" ]
 then
   # Fill index.jsx
   echo "import React from 'react'
-  import './index.css'
+import './index.css'
 
-  const $1 = () => {
-    return(
-      <div></div>
-    )
-  }
+const $1 = () => {
+  return(
+    <div></div>
+  )
+}
 
-  export default $1" >> src/components/$1/index.tsx
+export default $1" >> src/components/$1/index.tsx
 
   # Fill index.stories.jsx
   echo "import React from 'react';
-  import { storiesOf } from '@storybook/react';
-  import $1 from './index';
+import { storiesOf } from '@storybook/react';
+import $1 from './index';
 
-  storiesOf('$1', module).add('$1', () => {
-    return <$1 />;
-  });" >> src/components/$1/index.stories.tsx
+storiesOf('$1', module).add('$1', () => {
+  return <$1 />;
+});" >> src/components/$1/index.stories.tsx
 else
   echo "import React from 'react'
-  import './index.css'
+import './index.css'
 
-  type Props ={
-    $2
-  }
+type Props ={
+  $2
+}
 
-  const $1 = (props: Props) => {
-    return(
-      <div></div>
-    )
-  }
+const $1 = (props: Props) => {
+  return(
+    <div></div>
+  )
+}
 
-  export default $1" >> src/components/$1/index.tsx
+export default $1" >> src/components/$1/index.tsx
 
   echo "import React from 'react';
   import { storiesOf } from '@storybook/react';
