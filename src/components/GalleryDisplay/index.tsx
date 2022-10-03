38ddbae7452b0a08c02ed150e0d6dc72d45505cf
Scott Young <scott.young@getbridge.com>
2022-10-03 11:26:25 -0600

    gallery working with placeholder images

--- src/components/GalleryDisplay/index.tsx
+++ src/components/GalleryDisplay/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import './index.css'
 
-const GalleryDisplay = () => {
+type Props = {
+  images: string[]
+}
+
+const GalleryDisplay = (props: Props) => {
+  console.log(props.images)
   return (
     <div className="gallery-display">
-      <div>one</div>
-      <div>two</div>
-      <div>three</div>
-      <div>four</div>
-      <div>five</div>
-      <div>six</div>
+      {props.images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt="scott and alisia engagement"
+          className="gallery-image"
+        />
+      ))}
     </div>
   )
 }
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -3,6 +3,13 @@ import SectionHeader from '../components/SectionHeader'
 import GalleryDisplay from '../components/GalleryDisplay'
 
 const Gallery = () => {
+  const importImages = (r) => {
+    return r.keys().map(r)
+  }
+
+  const listOfImages = importImages(
+    require.context('./gallery-images/', false, /\.(png|jpe?g|svg)$/),
+  )
   return (
     <div id="gallery">
       {process.env.NODE_ENV === 'production' ? (
@@ -13,7 +20,7 @@ const Gallery = () => {
           <SectionHeader title="About Us" />
           <span>content</span>
           <SectionHeader title="Photo Gallery" />
-          <GalleryDisplay />
+          <GalleryDisplay images={listOfImages} />
           <h2>Have more images of the wedding you'd like to share?</h2>
           <h3>email them to us at photos@scottandalisia.com</h3>
         </>
