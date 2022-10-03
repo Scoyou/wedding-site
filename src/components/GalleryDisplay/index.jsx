a75955752d04f9c1618cefd73b7cf6aa1168e60a
Scott Young <scott.young@getbridge.com>
2022-10-03 08:57:42 -0600

    initial GalleryDisplay setup

---
+++ src/components/GalleryDisplay/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GalleryDisplay = () => {
+  return <div></div>;
+};
+
+export default GalleryDisplay;
---
+++ src/components/GalleryDisplay/index.stories.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import GalleryDisplay from "./index";
+
+storiesOf("GalleryDisplay", module).add("GalleryDisplay", () => {
+  return <GalleryDisplay />;
+});
