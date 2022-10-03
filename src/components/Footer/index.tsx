e9651039aeac088f552d3efddf71bbf5eeac8432
Scott Young <scott.young@getbridge.com>
2022-10-03 12:15:11 -0600

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const Footer = () => {
+  return(
+    <div className="footer">
+      <ul>
+        <li>Contact us with any questions at rsvp@scottandalisia.com</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
