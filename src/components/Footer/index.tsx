0076156817c701085acb2205563eb8a48e3a4c0b
Scott Young <scott.young@getbridge.com>
2022-10-18 14:27:18 -0600

    fine print

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,17 +7,14 @@ const Footer = () => {
       <p>Email us with any questions at {process.env.REACT_APP_RSVP_EMAIL}</p>
       <br />
       <p className="copyright">
-        Copyright Scott Young &copy; 2022, all rights reserved. <br />
+        &copy; Scott Young 2022, all rights reserved. <br />
         All images used on scottandalisia.com are the property of either
         scottandalisia.com or{' '}
-        <a
-          style={{ color: 'white' }}
-          href="https://www.facebook.com/shennilleshereephotography"
-        >
+        <a href="https://www.facebook.com/shennilleshereephotography">
           Shennile Sheree Photography
         </a>{' '}
         and cannot be used, modified, reproduced, or redistributed without the
-        express permission of the owner or owners.
+        permission of the owner or owners.
       </p>
     </div>
   )
